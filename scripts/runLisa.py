d1a9938b06969be64c76c38ac698a3e21ce82b6d
Yiheng Xu <yhxu@umd.edu>
2022-05-11 11:03:43 -0400

    run lisa with human genome

--- scripts/runLisa.py
+++ scripts/runLisa.py
@@ -17,27 +17,37 @@ refs = [
         "Pseudomonas",
     ]
 
+human = [
+        "Human20",
+        "Human21",
+    ]
+
 def main():
     pass
     # buildLisa()
     runLisa()
 
 def buildLisa():
-    for k in [4, 8, 16]:
-        for numLeafNode in [2**8, 2**12, 2**14, 2**16, 2**18, 2**22, 2**24, 2**26]:
-            for ref in refs:
-                command = '/nfshomes/yhxu/scratch/858D/Trans-Omics-Acceleration-Library/LISA/build-index-forward-only-lisa.o'
-                command += ' /nfshomes/yhxu/scratch/858D/data/fasta/' + ref + '.fasta ' 
+    # for k in [4, 8, 16]:
+    for k in [16]:
+        # for numLeafNode in [2**8, 2**10, 2**12, 2**14, 2**16, 2**18, 2**22, 2**20, 2**24, 2**26]:
+        for numLeafNode in [2**20]:
+            for ref in human:
+                command = '/nfshomes/yhxu/scratch/858D/858D-project/Trans-Omics-Acceleration-Library/LISA/build-index-forward-only-lisa.o'
+                command += ' /nfshomes/yhxu/scratch/858D/858D-project/data/fasta/' + ref + '.fasta ' 
                 command += str(k) + ' ' + str(numLeafNode)
                 command += ' &> /nfshomes/yhxu/scratch/858D/858D-project/result/build/' + ref + '.' + str(k) + '.' + str(numLeafNode)
                 subprocess.run(command, shell=True)
 
 def runLisa():
     subprocess.run('export OMP_NUM_THREADS=16', shell=True)
-    for k in [4, 8, 16]:
-        for numLeafNode in [2**10, 2**20]:
-            for numThreads in [1,2,4,8,16]:
-                for ref in refs:
+    # for k in [4, 8, 16]:
+    for k in [16]:
+        # for numLeafNode in [2**8, 2**10, 2**12, 2**14, 2**16, 2**18, 2**20, 2**22, 2**24, 2**26]:
+        for numLeafNode in [2**20]:
+            # for numThreads in [1,2,4,8,16]:
+            for numThreads in [1]:
+                for ref in human:
                     for queryLen in [32, 64, 96, 128]:
                         command = '/nfshomes/yhxu/scratch/858D/Trans-Omics-Acceleration-Library/LISA/exact-search-lisa.o'
                         command += ' /nfshomes/yhxu/scratch/858D/data/fasta/' + ref + '.fasta' 
