1b27f097bc49b770047741a80143be4a5e7bf550
Yiheng Xu <yhxu@umd.edu>
2022-05-11 20:58:41 -0400

    modify scripts

--- scripts/runLisa.py
+++ scripts/runLisa.py
@@ -9,7 +9,7 @@ import sys
 import math
 import time
 
-refs = [
+prokaryotics = [
         "Chlamydia",
         "Coxiella",
         "Caulobacter",
@@ -27,13 +27,20 @@ human = [
 
 def main():
     pass
-    buildLisa()
-    runLisa()
+    runLisaProkaryotics()
 
-def buildLisa():
-    # for k in [4, 8, 16]:
+def buildLisaProkaryotics():
+    for k in [4, 8, 16]:
+        for numLeafNode in [2**8, 2**10, 2**12, 2**14, 2**16, 2**18, 2**22, 2**20, 2**24, 2**26]:
+            for ref in prokaryotics:
+                command = '/nfshomes/yhxu/scratch/858D/Trans-Omics-Acceleration-Library/LISA/build-index-forward-only-lisa.o'
+                command += ' /nfshomes/yhxu/scratch/858D/data/fasta/' + ref + '.fasta ' 
+                command += str(k) + ' ' + str(numLeafNode)
+                command += ' &> /nfshomes/yhxu/scratch/858D/858D-project/result/build/' + ref + '.' + str(k) + '.' + str(numLeafNode)
+                subprocess.run(command, shell=True)
+
+def buildLisaHuman():
     for k in [16]:
-        # for numLeafNode in [2**8, 2**10, 2**12, 2**14, 2**16, 2**18, 2**22, 2**20, 2**24, 2**26]:
         for numLeafNode in [2**20]:
             for ref in human:
                 command = '/nfshomes/yhxu/scratch/858D/Trans-Omics-Acceleration-Library/LISA/build-index-forward-only-lisa.o'
@@ -42,13 +49,27 @@ def buildLisa():
                 command += ' &> /nfshomes/yhxu/scratch/858D/858D-project/result/build/' + ref + '.' + str(k) + '.' + str(numLeafNode)
                 subprocess.run(command, shell=True)
 
-def runLisa():
+
+def runLisaProkaryotics():
     subprocess.run('export OMP_NUM_THREADS=16', shell=True)
-    # for k in [4, 8, 16]:
+    for k in [4, 8, 16]:
+        for numLeafNode in [2**8, 2**10, 2**12, 2**14, 2**16, 2**18, 2**20, 2**22, 2**24, 2**26]:
+            for numThreads in [1,2,4,8,16]:
+                for ref in prokaryotics:
+                    for queryLen in [32, 64, 96, 128]:
+                        command = '/nfshomes/yhxu/scratch/858D/Trans-Omics-Acceleration-Library/LISA/exact-search-lisa.o'
+                        command += ' /nfshomes/yhxu/scratch/858D/data/fasta/' + ref + '.fasta' 
+                        command += ' /nfshomes/yhxu/scratch/858D/data/query/' + ref + '.' + str(queryLen) + '.query '
+                        command += str(k) + ' ' + str(numLeafNode) + ' ' + str(numThreads)
+                        command += ' &> /nfshomes/yhxu/scratch/858D/858D-project/result/query/'
+                        command += ref + '.' + str(k) + '.' + str(numLeafNode) + '.' + str(numThreads) + '.' + str(queryLen)
+                        subprocess.run(command, shell=True)
+                    pass
+
+def runLisaHuman():
+    subprocess.run('export OMP_NUM_THREADS=16', shell=True)
     for k in [16]:
-        # for numLeafNode in [2**8, 2**10, 2**12, 2**14, 2**16, 2**18, 2**20, 2**22, 2**24, 2**26]:
         for numLeafNode in [2**20]:
-            # for numThreads in [1,2,4,8,16]:
             for numThreads in [1]:
                 for ref in human:
                     for queryLen in [32, 64, 96, 128]:
